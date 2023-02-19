dd58fa257dc8acd2cd2bc2ecbcd8acb470bb6838
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-19 15:20:33 +0900

    async/await

---
+++ chapter2/asyncVer.js
@@ -0,0 +1,10 @@
+const findAndSaveUser = async (Users) => {
+  try {
+    let user = await Users.findOne({});
+    user.name = "zero";
+    user = await user.save();
+    user = await Users.findOne({ gender: "m" });
+  } catch (error) {
+    console.error(error);
+  }
+};
