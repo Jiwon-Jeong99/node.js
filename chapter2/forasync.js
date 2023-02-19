0fb5feff3dbccdb4b5973ea708a4228b5c6183b9
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-19 15:30:08 +0900

    formData

---
+++ chapter2/forasync.js
@@ -0,0 +1,7 @@
+const promise1 = Promise.resolve("성공");
+const promise2 = Promise.resolve("성공2");
+(async () => {
+  for await (promise of [promise1, promise2]) {
+    console.log(promise);
+  }
+})();
