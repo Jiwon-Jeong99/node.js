c5918168d62d185e41bad305a86283849d10ed73
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-02-19 14:25:30 +0900

    arrow function

--- chapter2/this_binding.js
+++ chapter2/this_binding.js
@@ -1,13 +1,11 @@
 var relationship1 = {
-    name: 'zero',
-    friends: ['nero', 'hero', 'xero'],
-    logFriends: function() {
-        var that = this;
-        this.friends.forEach(function(friend){
-            console.log(that.name, friend);
-        });
-    },
+  name: "zero",
+  friends: ["nero", "hero", "xero"],
+  logFriends: function () {
+    var that = this;
+    this.friends.forEach((friend) => {
+      console.log(that.name, friend);
+    });
+  },
 };
 relationship1.logFriends();
-
-const 
