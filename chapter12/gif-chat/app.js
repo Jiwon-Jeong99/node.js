7e0772d45196e016415c143de151856e69bbbee8
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-03-01 14:49:21 +0900

    chapter12 : websocket

---
+++ chapter12/gif-chat/app.js
@@ -0,0 +1,57 @@
+//packages
+import express from "express";
+import path from "path";
+import morga from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import nunjucks from "nunjucks";
+import dotenv from "dotenv";
+
+//routes
+import indexRouter from "./routes";
+
+dotenv.config();
+
+app.set("port", process.env.PORT || 8005);
+app.set("view engine", "html");
+
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.statue(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
